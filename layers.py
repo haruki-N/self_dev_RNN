51abcdf602d30a1b3cad1da21cc201c7cf98d3af
Haruki Nagasawa <haruki.spring04@gmail.com>
2023-08-19 22:59:03 +0900

    mainly implemented rnnlm class

---
+++ layers.py
@@ -0,0 +1,93 @@
+import sys
+import os
+sys.path.append(os.path.abspath())
+from functions import *
+import numpy as np
+
+
+
+
+class Affine:
+    def __init___(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)  # 行列積の逆伝搬
+        dW = np.dot(self.x.T, dout)  # 行列積の逆伝搬
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+    
+
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+    
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
+    
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+
+        return loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1  # one-hot-vector に変換
+        dx *= dout
+        dx /= batch_size
+
+        return dx
+    
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+
+        return None
